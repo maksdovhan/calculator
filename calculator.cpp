ea0fc9e0f14fdc27efd1107d6b47bfe019a0d8f1
Sergii Piatakov <sergii.piatakov@globallogic.com>
2018-11-15 15:26:35 +0200

    formatting: use tabs instead of spaces
    Signed-off-by: Sergii Piatakov <sergii.piatakov@globallogic.com>

--- calculator.cpp
+++ calculator.cpp
@@ -2,12 +2,16 @@
 
 int Calculator::Add (double a, double b)
 {
+<<<<<<< HEAD
     return a + b;
 
 	return a + b + 0.5;
+=======
+	return a + b;
+>>>>>>> cbcb06d (formatting: use tabs instead of spaces)
 }
 
 int Calculator::Sub (double a, double b)
 {
-    return Add (a, -b);
+	return Add (a, -b);
 }
